155447fc9c25c6cb2eeb6fac2d83fb773e3b17d8
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-10-18 18:48:11 -0500

    inventory-progress

--- server/config/seeds.js
+++ server/config/seeds.js
@@ -19,12 +19,12 @@ db.once('open', async () => {
 
   const products = await Product.insertMany([
     {
-      name: 'Camisa Azul',
+      name: 'Camisa Boss',
       description:
         'Talla: Chica, Necesita una pequeña reparación en la manga',
-      image: 'special.jpg',
+      image: 'camisa_0.jpg',
       category: categories[0]._id,
-      price: 5.00,
+      price: 20.00,
       quantity: 1
     },
 
@@ -32,110 +32,165 @@ db.once('open', async () => {
       name: 'Camisa Blanca Formal',
       description:
         'Talla: Grande, Buenas condiciones y excelente para entrevistas de trabajo',
-      image: 'special-1.jpg',
+      image: 'camisa_1.jpg',
       category: categories[0]._id,
-      price: 15.00,
+      price: 20.00,
       quantity: 1
     },
 
     {
-      name: 'Pantalon de Mezclilla',
-      category: categories[1]._id,
+      name: 'Camisa de rayas',
+      category: categories[0]._id,
       description:
         'Talla: Mediana de hombre, buenas condiciones',
-      image: 'casual.jpg',
-      price: 30.00,
+      image: 'camisa_2.jpg',
+      price: 20.00,
       quantity: 1
     },
 
     {
-      name: 'Arrangement for One',
-      category: categories[1]._id,
+      name: 'Camisa Manchester',
+      category: categories[0]._id,
       description:
-        'Praesent placerat, odio vel euismod venenatis, lectus arcu laoreet felis, et fringilla sapien turpis vestibulum nisl.',
-      image: 'casual-1.jpg',
-      price: 3.99,
+        'Talla: Mediana de hombre, buenas condiciones',
+      image: 'camisa_3.jpg',
+      price: 50.00,
       quantity: 1
     },
 
     {
-      name: 'A Special Box For You',
+      name: 'Pantalon Capri',
       category: categories[1]._id,
       description:
-        'Vivamus ut turpis in purus pretium mollis. Donec turpis odio, semper vel interdum ut, vulputate at ex. Duis dignissim nisi vel tortor imperdiet finibus. Aenean aliquam sagittis rutrum.',
+        'Pantalon de Mujer talla mediana',
       image: 'casual-2.jpg',
-      price: 14.99,
+      price: 40.00,
       quantity: 1
     },
 
     {
-      name: 'Heart Display',
-      category: categories[2]._id,
+      name: 'Pantalon 3/4',
+      category: categories[1]._id,
       description:
-        'Vestibulum risus metus, luctus non tortor quis, tincidunt consectetur ex. Nullam vitae lobortis ligula, ut sagittis massa. Curabitur consectetur, tellus at pulvinar venenatis, erat augue cursus erat, eu ullamcorper eros lectus ultrices ipsum. Integer rutrum, augue vitae auctor venenatis, turpis turpis elementum orci, at sagittis risus mi a leo.',
-      image: 'wedding.jpg',
-      price: 399.99,
+        'Pantalon de Mujer talla mediana',
+      image: 'pantalon_0.jpg',
+      price: 40.00,
       quantity: 1
     },
 
     {
-      name: 'Bouquet Vintage',
-      category: categories[2]._id,
+      name: 'Pantalon de mujer grande',
+      category: categories[1]._id,
       description:
-        'In sodales, ipsum quis ultricies porttitor, tellus urna aliquam arcu, eget venenatis purus ligula ut nisi. Fusce ut felis dolor. Mauris justo ante, aliquet non tempus in, tempus ac lorem. Aliquam lacinia dolor eu sem eleifend ultrices. Etiam mattis metus metus. Sed ligula dui, placerat non turpis vitae, suscipit volutpat elit. Phasellus sagittis, diam elementum suscipit fringilla, libero mauris scelerisque ex, ac interdum diam erat non sapien.',
-      image: 'wedding-2.jpg',
-      price: 199.99,
+        'Muy comodo para ajustar la lonja',
+      image: 'pantalon_1.jpg',
+      price: 40.00,
       quantity: 1
     },
 
     {
-      name: 'Roses at Bedtime',
-      category: categories[3]._id,
+      name: 'Pantalon de Mezclilla para ninas',
+      category: categories[1]._id,
       description:
-        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ornare diam quis eleifend rutrum. Aliquam nulla est, volutpat non enim nec, pharetra gravida augue. Donec vitae dictum neque. Pellentesque arcu lorem, fringilla non ligula ac, tristique bibendum erat. Ut a semper nibh. Quisque a mi et mi tempor ultricies. Maecenas eu ipsum eu enim hendrerit accumsan at euismod urna.',
-      image: 'wedding-3.jpg',
-      price: 9.99,
+        'Pantalon de mezclilla para mujercitas',
+      image: 'pantalon_2.jpg',
+      price: 20.00,
       quantity: 1
     },
 
     {
       name: 'Spinning Rose Center Table',
-      category: categories[4]._id,
+      category: categories[1]._id,
       description: 'Ut vulputate hendrerit nibh, a placerat elit cursus interdum.',
-      image: 'sorry.jpg',
+      image: 'pantalon_3.jpg',
       price: 1.99,
       quantity: 1
     },
 
     {
-      name: 'Set of Orchids',
-      category: categories[4]._id,
+      name: 'Tacones',
+      category: categories[2]._id,
       description:
-        'Sed a mauris condimentum, elementum enim in, rhoncus dui. Phasellus lobortis leo odio, sit amet pharetra turpis porta quis.',
-      image: 'sorry-1.jpg',
-      price: 2.99,
+        'Sexy y en buenas condiciones.',
+      image: 'zapatos_0.jpg',
+      price: 60.00,
       quantity: 1
     },
 
     {
-      name: 'Funeral',
-      category: categories[4]._id,
+      name: 'Zapatos cafes',
+      category: categories[2]._id,
       description:
-        'Vestibulum et erat finibus erat suscipit vulputate sed vitae dui. Ut laoreet tellus sit amet justo bibendum ultrices. Donec vitae felis vestibulum, congue augue eu, finibus turpis.',
-      image: 'special-3.jpg',
-      price: 7.99,
+        'Comodos y para trabajar.',
+      image: 'zapatos_1.jpg',
+      price: 30.00,
       quantity: 1
     },
 
     {
-      name: 'White Roses',
-      category: categories[4]._id,
+      name: 'Zapato cerrado azul',
+      category: categories[2]._id,
       description:
-        'Morbi consectetur viverra urna, eu fringilla turpis faucibus sit amet. Suspendisse potenti. Donec at dui ac sapien eleifend hendrerit vel sit amet lectus.',
-      image: 'funeral.jpg',
-      price: 9.99,
+        'Listos y ajustados para largas caminatas.',
+      image: 'zapatos_2.jpg',
+      price: 30.00,
       quantity: 1
-    }
+    },
+
+    {
+      name: 'Pato Donald',
+      category: categories[3]._id,
+      description:
+        'Comodos y para trabajar.',
+      image: 'zapatos_1.jpg',
+      price: 30.00,
+      quantity: 1
+    },
+    {
+      name: 'Zapatos cafes',
+      category: categories[3]._id,
+      description:
+        'Comodos y para trabajar.',
+      image: 'zapatos_1.jpg',
+      price: 30.00,
+      quantity: 1
+    },
+    {
+      name: 'Zapatos cafes',
+      category: categories[3]._id,
+      description:
+        'Comodos y para trabajar.',
+      image: 'zapatos_1.jpg',
+      price: 30.00,
+      quantity: 1
+    },
+    {
+      name: 'Zapatos cafes',
+      category: categories[3]._id,
+      description:
+        'Comodos y para trabajar.',
+      image: 'zapatos_1.jpg',
+      price: 30.00,
+      quantity: 1
+    },
+    {
+      name: 'Zapatos cafes',
+      category: categories[3]._id,
+      description:
+        'Comodos y para trabajar.',
+      image: 'zapatos_1.jpg',
+      price: 30.00,
+      quantity: 1
+    },
+    {
+      name: 'Zapatos cafes',
+      category: categories[3]._id,
+      description:
+        'Comodos y para trabajar.',
+      image: 'zapatos_1.jpg',
+      price: 30.00,
+      quantity: 1
+    },
   ]);
 
   console.log('products seeded');
